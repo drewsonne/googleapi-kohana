7dcb568c0934609d036f38a4b943092c6919ec17
Drew J. Sonne <drew.sonne@gmail.com>
2013-10-19 19:22:24 +1100

    Fixed bad auth.

--- classes/Controller/GoogleAPI/Auth.php
+++ classes/Controller/GoogleAPI/Auth.php
@@ -11,22 +11,22 @@ class Controller_GoogleAPI_Auth extends Controller {
             if($gplus->client->isAccessTokenExpired()) {
                 session_destroy();
                 $newAccessToken = json_decode($gplus->client->getAccessToken());
-//                var_dump($newAccessToken); die();
                 $gplus->client->refreshToken($newAccessToken->refresh_token);
             }
 
             $config = Kohana::$config->load('googleapi');
-
             $isLoggedInSession = $config->get('is_logged_in', null);
+            $redirectUrl = $config->get('post_auth_url', null);
+
             if(!is_null($isLoggedInSession)) {
                 $user = $gplus->getUser();
                 \Kohana_Session::instance()->set($isLoggedInSession, true);
             }
 
-            $redirectUrl = $config->get('post_auth_url', null);
             if(!is_null($redirectUrl)) {
                 $redirectUrl = Request::$current->detect_uri();
             }
+
 			HTTP::redirect($redirectUrl);
 		}
 		else
